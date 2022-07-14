2d846f830000f8544f591f5a0793fc47667c8c20
Julio Lima <lima.julio.cs@gmail.com>
2022-07-14 13:33:14 -0300

    Date select

--- backend/src/main/java/com/devsuperior/dsmeta/SaleController.java
+++ backend/src/main/java/com/devsuperior/dsmeta/SaleController.java
@@ -2,11 +2,12 @@ package com.devsuperior.dsmeta;
 
 import com.devsuperior.dsmeta.entities.Sale;
 import com.devsuperior.dsmeta.services.SaleService;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
 
 @RestController
 @RequestMapping(value = "/api/v1/sales")
@@ -19,7 +20,10 @@ public class SaleController {
     }
 
     @GetMapping(value = "/all")
-    public List<Sale> findSales() {
-        return saleService.findSales();
+    public Page<Sale> findSales(
+            @RequestParam(value = "minDate", defaultValue = "") String minDate,
+            @RequestParam(value = "maxDate", defaultValue = "") String maxDate,
+            Pageable pageable) {
+        return saleService.findSales(minDate, maxDate, pageable);
     }
 }
--- backend/src/main/java/com/devsuperior/dsmeta/services/SaleService.java
+++ backend/src/main/java/com/devsuperior/dsmeta/services/SaleService.java
@@ -2,9 +2,11 @@ package com.devsuperior.dsmeta.services;
 
 import com.devsuperior.dsmeta.entities.Sale;
 import com.devsuperior.dsmeta.repository.SaleRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
+import java.time.LocalDate;
 
 @Service
 public class SaleService {
@@ -15,7 +17,11 @@ public class SaleService {
         this.saleRepository = saleRepository;
     }
 
-    public List<Sale> findSales() {
-        return saleRepository.findAll();
+    public Page<Sale> findSales(String minDate, String maxDate, Pageable pageable) {
+
+        LocalDate min = LocalDate.parse(minDate);
+        LocalDate max = LocalDate.parse(maxDate);
+
+        return saleRepository.findSales(min, max, pageable);
     }
 }
