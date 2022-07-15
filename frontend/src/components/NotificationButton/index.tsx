baffcaf5fb9e5ed11836e170057e7ef9cc24b592
Julio Lima <lima.julio.cs@gmail.com>
2022-07-15 13:24:58 -0300

    Send notification

--- frontend/src/components/NotificationButton/index.tsx
+++ frontend/src/components/NotificationButton/index.tsx
@@ -1,10 +1,22 @@
+import axios from 'axios';
 import Icon from '../../assets/images/notification-icon.svg';
+import { BASE_URL } from '../../utils/request';
 
 import './styles.css';
 
-export function NotificationButton() {
+type NotificationButtonProps = {
+  saleId: number;
+}
+
+export function NotificationButton({ saleId } : NotificationButtonProps) {
+
+  function handleSendNotification(id: number) {
+    axios.get(`${BASE_URL}/api/v1/sales/${id}/notification`)
+      .then(response => { console.log("mensagem enviada") });
+  }
+
   return(
-    <div className="dsmeta-red-btn">
+    <div className="dsmeta-red-btn" onClick={() => handleSendNotification(saleId)}>
       <img src={Icon} alt="Notification button" />
     </div>
   );
